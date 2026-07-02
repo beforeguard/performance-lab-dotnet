b611d1ff3306a797104e4e30adfa0ec22015c940
Brian Egan <beforeguard@gmail.com>
2026-07-01 18:11:31 -0700

    Add load testing project and configuration for NBomber

---
+++ tools/PerformanceLab.LoadTests/Program.cs
@@ -0,0 +1,30 @@
+﻿using NBomber.CSharp;
+
+var httpClientHandler = new HttpClientHandler
+{
+    ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+};
+
+using var httpClient = new HttpClient(httpClientHandler);
+
+var scenario = Scenario.Create(
+    "users_baseline",
+    async context =>
+    {
+        var response = await httpClient.GetAsync(
+            "https://localhost:5206/users");
+
+        return response.IsSuccessStatusCode
+            ? Response.Ok()
+            : Response.Fail();
+    })
+    .WithLoadSimulations(
+        Simulation.Inject(
+            rate: 50,
+            interval: TimeSpan.FromSeconds(1),
+            during: TimeSpan.FromMinutes(1)));
+
+NBomberRunner
+    .RegisterScenarios(scenario)
+    .Run();
